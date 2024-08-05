841572175f0c399377f46005d01c9bd7bcb656fe
jmnoland <jody@noreply.com>
2024-08-05 20:24:10 +0200

    Event get and add functions

--- model/event.go
+++ model/event.go
@@ -3,14 +3,15 @@ package model
 import (
     "github.com/google/uuid"
     "time"
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Event struct {
-    ID uuid.UUID
-    Application string
-    Action string
-    Type string
-    Request string
+    ID              primitive.ObjectID   `json:"id" bson:"_id,omitempty"`
+    Application     string
+    Action          string
+    Type            string
+    Request         string
     ActionRequestId uuid.UUID
 
     CreatedAt time.Time
--- repository/event.go
+++ repository/event.go
@@ -3,19 +3,20 @@ package repository
 import (
     "context"
     "os"
+    "time"
 
+    "github.com/jmnoland/identity/model"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var uri = os.Getenv("MONGO_DB_CONNECTION")
-
+var database = os.Getenv("MONGO_DB_NAME")
+var serverAPI = options.ServerAPI(options.ServerAPIVersion1)
+var opts = options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
 func GetEvents() {
-    serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-    opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
     client, err := mongo.Connect(context.TODO(), opts)
 
     if err != nil {
@@ -27,11 +28,46 @@ func GetEvents() {
         }
     }()
 
-    var result bson.M
-    if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    events := client.Database(database).Collection("events")
+    cur, err := events.Find(context.TODO(), bson.D{})
+    if err != nil {
         panic(err)
     }
+    defer cur.Close(ctx)
+    
+    var eventList []model.Event = make([]model.Event, 0)
+    for cur.Next(ctx) {
+        var result model.Event
+        err := cur.Decode(&result)
+        if err != nil {
+            panic(err)
+        }
+        eventList = append(eventList, result)
 
+    }
+    
 }
 
+func AddEvent(newEvent model.Event) {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    client, err := mongo.Connect(ctx, opts)
+    if err != nil {
+        panic(err)
+    }
+    defer func() {
+        if err = client.Disconnect(ctx); err != nil {
+            panic(err)
+        }
+    }()
+    defer cancel()
+
+    events := client.Database(database).Collection("events")
+    _, err = events.InsertOne(ctx, newEvent)
+    if err != nil {
+        panic(err)
+    }
+}
 
